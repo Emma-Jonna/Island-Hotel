48bbea8bc702db3d5979f3d87886c79d37fb8bc0
Emma-Jonna <johanna.emma.pihl@gmail.com>
2022-12-16 19:35:24 +0100

    added all the font faces

--- index.php
+++ index.php
@@ -8,14 +8,30 @@
     <link rel="stylesheet" href="/CSS/global.css">
     <link rel="stylesheet" href="/CSS/main.css">
     <link rel="stylesheet" href="/CSS/typography.css">
+    <link rel="stylesheet" href="/CSS/fonts.css">
     <title>Hotel</title>
 </head>
 
 <body>
+    <div class="font-tests">
+        <h1>UnifrakturCook</h1>
+        <p class="one">Vollkorn-Regular</p>
+        <p class="two">Vollkorn-Regular-Italic</p>
+        <p class="three">Vollkorn-Medium</p>
+        <p class="four">Vollkorn-Medium-Italic</p>
+        <p class="five">Vollkorn-SemiBold</p>
+        <p class="six">Vollkorn-SemiBold-Italic</p>
+        <p class="seven">Vollkorn-Bold</p>
+        <p class="eight">Vollkorn-Bold-Italic</p>
+        <p class="nine">Vollkorn-ExtraBold</p>
+        <p class="ten">Vollkorn-ExtraBold-Italic</p>
+        <p class="eleven">Vollkorn-Black</p>
+        <p class="twelve">Vollkorn-Black-Italic</p>
+    </div>
     <img src="/Images/le-mont-saint-michel-by-night.jpg" alt="">
-    <img src="/Images/Best-Beaches-near-Mont-Saint-Michel.jpg" alt="">
-    <img src="/Images/895_0_mon-sen-mishel-smapse1.jpg" alt="">
-    <img src="/Images/castle.jpeg" alt="">
+    <!-- <img src="/Images/Best-Beaches-near-Mont-Saint-Michel.jpg" alt=""> -->
+    <!-- <img src="/Images/895_0_mon-sen-mishel-smapse1.jpg" alt=""> -->
+    <!-- <img src="/Images/castle.jpeg" alt=""> -->
 </body>
 
 </html>
