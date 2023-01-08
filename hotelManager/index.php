34a6e597f31610f331aee24cadb0b9ac307b6777
Emma-Jonna <johanna.emma.pihl@gmail.com>
2023-01-08 15:35:23 +0100

    added admin login files

---
+++ hotelManager/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./CSS/global.css">
+    <link rel="stylesheet" href="./CSS/main.css">
+    <link rel="stylesheet" href="./CSS/typography.css">
+    <link rel="stylesheet" href="./CSS/fonts.css">
+    <title>Hotel</title>
+</head>
+
+<body>
+    <h1>hello</h1>
+</body>
+
+</html>
